827879399afd93a5d1309963f47fa48287c2f07a
AmanyNabil <amani.nabil@hotmail.com>
2022-07-18 01:18:26 +0200

    Update deploy.sh

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,7 +1,7 @@
-npm run build
-eb init udagram-api --region us-east-1 --platform node.js
-eb use udagram-api-env
-eb deploy udagram-api-env
+# npm run build
+# eb init udagram-api --region us-east-1 --platform node.js
+# eb use udagram-api-env
+# eb deploy udagram-api-env
 
 aws s3 cp --recursive --acl public-read ./www s3://myawsbucket-75139724085/
 aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://myawsbucket-75139724085/
